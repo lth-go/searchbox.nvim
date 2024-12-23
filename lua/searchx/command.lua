d20d26d2c8419a92c944d2a5099f68c4ddb149ac
李天华 <litianhua@moresec.cn>
2024-12-23 10:01:49 +0800

    fix

--- lua/searchx/command.lua
+++ lua/searchx/command.lua
@@ -1,9 +1,30 @@
 local input = require("searchx.inputs")
 local search = require("searchx.search")
 
+local buf_is_valid = function(bufnr)
+  if not vim.api.nvim_buf_is_valid(bufnr) then
+    return false
+  end
+
+  if vim.api.nvim_get_option_value("buftype", { buf = bufnr }) ~= "" then
+    return false
+  end
+
+  if vim.api.nvim_get_option_value("bufhidden", { buf = bufnr }) ~= "" then
+    return false
+  end
+
+  return true
+end
+
 local M = {}
 
 M.search_raw = function()
+  if not buf_is_valid(vim.api.nvim_get_current_buf()) then
+    vim.api.nvim_feedkeys("/", "n", false)
+    return
+  end
+
   input.search(search.search_raw)
 end
 
